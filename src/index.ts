824296f3e6d3f353db51d520806fef90a0d9c65a
Rafael Galvez Ruiz <rgalvezr@vectoritcgroup.com>
2021-04-14 11:59:51 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Observable, Observer } from 'rxjs';
+
+
+const observer:Observer<any> = {
+    next: value => console.log('siguiente [next]:',value),
+    error: error => console.log('error [obs]',error),
+    complete: () => console.info('completador [nose]')
+}
+
+const obs$ = new Observable<string>(subs =>{
+    subs.next('Hola');
+    subs.next('Hola');
+    subs.next('Hola');
+});
+
+
+obs$.subscribe(observer);
+
+
+// obs$.subscribe(
+//     valor => console.log('next: ',valor),
+//     error => console.log('error',error ),
+//     () => console.log('Completadoo amigo')
+// );
